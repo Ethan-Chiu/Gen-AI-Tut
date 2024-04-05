baed0935dbb3f71d68e327f74a28527dece73efb
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-04-05 13:46:46 +0800

    Match manager

---
+++ api/manager.py
@@ -0,0 +1,31 @@
+from .models import Match
+from .server import Server
+
+class Manager:
+    def __init__(self, server: Server):
+        self.server = server
+        self.current_match_id = None
+
+    def set_current_match(self, match_id):
+        self.current_match_id = match_id
+
+    def get_current_match(self) -> Match | None:
+        match_data = self.server.get_match(self.current_match_id)
+        if match_data:
+            match = Match.from_json(match_data)
+            return match
+        else:
+            print("Failed to get current match.")
+            return None
+        
+
+    def send_message(self, message):
+        if self.current_match_id is None:
+            print("No current match ID set. Use get_current_match() first.")
+            return
+
+        response = self.server.send_message(self.current_match_id, message)
+        if response:
+            print("Message sent successfully.")
+        else:
+            print("Failed to send message.")
